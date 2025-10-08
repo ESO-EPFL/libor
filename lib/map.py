57f72edce0c71fd8045ecfdc68aa982935410cf8
AurelienBrn <aurelien.brun@epfl.ch>
2025-10-08 10:32:26 +0200

    Correspondence class

--- lib/map.py
+++ lib/map.py
@@ -25,10 +25,9 @@ class Trajectory:
     def __init__(self, t, lla, rpy, tp):
         self.t = t
 
-        self.lla = lla
+        self.lla = lla.reshape(-1,3)
         self.ecef = np.dstack(lla2ecefTransformer.transform(lla[:, 0], lla[:, 1], lla[:, 2],radians=True))[0]
         self.xyz = tp.R_ecef2enu @ (self.ecef - tp.xyz0).T
-
 
         R_nedi2b = []
         R_nedi2e = []
@@ -37,19 +36,76 @@ class Trajectory:
             R_nedi2b.append(R_ned2b(rpy[i,0], rpy[i,1], rpy[i,2]))
             R_nedi2e.append(R_ned2e(lla[i,0], lla[i,1]))
 
-
         self.R_nedi2b = R.from_matrix(np.array(R_nedi2b))
         self.R_nedi2e = R.from_matrix(np.array(R_nedi2e))
 
         self.slerp_nedi2b = Slerp(t, self.R_nedi2b)
         self.slerp_nedi2e = Slerp(t, self.R_nedi2e)
         
-    def interpolate(self, timestamps):
+    def interpolate(self, timestamps, customRPY = True):
         xyz_interp = np.empty((3, len(timestamps)))
+        lla_interp = np.empty((len(timestamps),3))
         for i in range(3):
             xyz_interp[i,:] = np.interp(timestamps, self.t, self.xyz[i,:])
-        
-        R_ned2b_interp = self.slerp_nedi2b(timestamps)
-        R_ned2e_interp = self.slerp_nedi2e(timestamps)
+            lla_interp[i,:] = np.interp(timestamps, self.t, self.lla[i,:])
 
-        return xyz_interp, R_ned2b_interp, R_ned2e_interp
+        R_nedi2b_interp = self.slerp_nedi2b(timestamps)
+        R_nedi2e_interp = self.slerp_nedi2e(timestamps).as_matrix()
+
+        if customRPY:
+            rpy_interp = np.empty((len(timestamps),3))
+            for i in range(len(timestamps)):
+                rpy_interp[i,:] = rpy_from_R_ned2b(R_nedi2b_interp[i].as_matrix(), as_degrees=True)
+        else:
+            rpy_interp = R_nedi2b_interp.as_euler('xyz', degrees=True)
+
+
+        return xyz_interp, R_nedi2b_interp, R_nedi2e_interp, rpy_interp
+    
+def loadSBET(path):
+    """
+    Decodes an APPLANIX SNV/SBET file.
+
+    Parameters:
+    - settings: path to SBET
+
+    Returns:
+    - data: numpy array of processed data
+
+  Input record: 17xdouble=(136 bytes)
+       0  time  			sec_of_week 
+       1  latitude   		rad
+       2  longitude  		rad
+       3  altitude       meters
+       4  x_wander_vel   m/s
+       5  y_wander_vel   m/s
+       6  z_wander_vel  	m/s
+       7  roll          	radians
+       8  pitch         	radians
+       9  wander_heading radians
+       10 wander angle   radians
+       11 x body accel   m/s^2
+       12 y body accel   m/s^2
+       13 z body accel   m/s^2
+       14 x angular rate rad/s
+       15 y angular rate rad/s
+	   16 z angular rate rad/s					
+ This is what is written in the ouput record:
+       0   time            sec_of_week
+       1   latitude        rad
+       2   longitude       rad
+       3   altitude        m
+       4   roll            rad
+       5   pitch           rad
+       6  heading         rad 
+    """
+
+    try:
+        with open(path, "rb") as f:
+            print(f"Loading file {path}")
+            data = np.fromfile(f, dtype=np.float64).reshape(-1,17)
+    except Exception as e:
+        errmsg = f"Cannot open file! {str(e)}"
+        raise ValueError(errmsg)
+        
+    return data[:, 0], data[:, 1:4],  np.column_stack((data[:, 7:9], data[:, 9]-data[:, 10]))
--- lib/model.py
+++ lib/model.py
@@ -3,11 +3,91 @@ from lib.rotations import *
 
 
 class Correspondence:
-    def __init__(self, corr, trj, R_lidar2b, leverArm):
-        self.ti = corr[0]
-        self.tj = corr[1]
+    def __init__(self, corr, trj, mount, R_e2enu):
+        self.t_i = corr[0]
+        self.t_j = corr[1]
 
-        
+        self.R_s2b = mount.R_s2b
+        self.leverArm = mount.leverArm
 
+        xyz_i, _, R_ned2e_i, rpy_i = trj.interpolate(self.t_i, customRPY=True)
+        xyz_j, _, R_ned2e_j, rpy_j = trj.interpolate(self.t_j, customRPY=True)
 
-        
+        self.l_ij = np.hstack((xyz_i, rpy_i, xyz_j, rpy_j)).reshape(12,1)
+        self.l_ij_hat = np.empty((12,1))
+
+        self.u_i = self.R_s2b @ corr[2:5].reshape(3,1)
+        self.u_j = self.R_s2b @ corr[5:8].reshape(3,1)
+
+        self.U_i = skewT(self.u_i)
+        self.U_j = skewT(self.u_j)
+
+        self.v_ij = np.zeros((12,1))
+
+        self.R_e2enu = R_e2enu
+
+        self.R_ned2e_i = R_ned2e_i
+        self.R_ned2e_j = R_ned2e_j 
+
+        self.A = np.zeros((3,3))
+        self.B = np.zeros((3,12))
+        self.w = np.zeros((3,1))
+        self.p_i = np.zeros((3,1))
+        self.p_j = np.zeros((3,1))
+        self.P = np.zeros((12,12))
+
+    def l_ij_hat(self):
+        self.l_ij_hat =  self.l_ij + self.v_ij
+
+    def compute_Rb2m(self):
+        r_i, p_i, y_i = self.l_ij_hat[3:6]
+        r_j, p_j, y_j = self.l_ij_hat[9:12]
+
+        R_b2ned_i = R_b2ned(r_i, p_i, y_i)
+        R_b2ned_j = R_b2ned(r_j, p_j, y_j)
+
+        self.R_b2m_i = self.R_e2enu @ self.R_ned2e_i @ R_b2ned_i
+        self.R_b2m_j = self.R_e2enu @ self.R_ned2e_j @ R_b2ned_j
+
+    def computeA(self):
+        self.A = self.R_b2m_i @ self.U_i - self.R_b2m_j @ self.U_j
+
+    def computeB(self, theta):
+
+        r_i, p_i, y_i = self.l_ij_hat[3:6]
+        r_j, p_j, y_j = self.l_ij_hat[9:12]
+
+        s_i = self.u_i + self.U_i @ theta + self.mount.leverArm
+        s_j = self.u_j + self.U_j @ theta + self.mount.leverArm
+
+        dR_dr_i = dR_b2ned_dr(r_i, p_i, y_i)
+        dR_dp_i = dR_b2ned_dp(r_i, p_i, y_i)
+        dR_dy_i = dR_b2ned_dy(r_i, p_i, y_i)
+
+        dR_dr_j = dR_b2ned_dr(r_j, p_j, y_j)
+        dR_dp_j = dR_b2ned_dp(r_j, p_j, y_j)
+        dR_dy_j = dR_b2ned_dy(r_j, p_j, y_j)
+
+        B0 = np.eye(3)
+        B1 = dR_dr_i @ s_i
+        B2 = dR_dp_i @ s_i
+        B3 = dR_dy_i @ s_i
+        B4 = -np.eye(3)
+        B5 = -dR_dr_j @ s_j
+        B6 = -dR_dp_j @ s_j
+        B7 = -dR_dy_j @ s_j
+
+        self.B = self.R_e2enu @ np.hstack((B0, B1, B2, B3, B4, B5, B6, B7))
+
+    def compute_w(self, theta):
+        borVector_i = self.U_i @ theta
+        borVector_j = self.U_j @ theta
+
+        self.p_i = np.squeeze(self.l_ij_hat[0:3].reshape(3,1) + self.R_b2m_i @ (self.u_i + borVector_i + self.leverArm))
+        self.p_j = np.squeeze(self.l_ij_hat[6:9].reshape(3,1) + self.R_b2m_j @ (self.u_j + borVector_j + self.leverArm))
+
+        self.w = self.p_i - self.p_j
+
+    def computeP(self, sigma_xy, sigma_z, sigma_rp, sigma_y):
+        self.P = np.diag([sigma_xy**2, sigma_xy**2, sigma_z**2, sigma_rp**2, sigma_rp**2, sigma_y**2,
+                          sigma_xy**2, sigma_xy**2, sigma_z**2, sigma_rp**2, sigma_rp**2, sigma_y**2])
